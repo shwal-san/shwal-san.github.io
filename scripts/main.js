b4d9a72924f1cef250339000a000fc5541700296
shwal-san <ves232315@gmail.com>
2025-10-03 18:57:59 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/hills.png") {
+    myImage.setAttribute("src", "images/blue-peaks.png");
+  } else {
+    myImage.setAttribute("src", "images/hills.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mountain peaks are blue, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mountain peaks are blue, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
